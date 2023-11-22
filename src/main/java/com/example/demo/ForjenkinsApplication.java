e4f7b5170f13b3d0b2db63acba489b761474e098
manojdude <68345729+manojdude@users.noreply.github.com>
2023-11-22 19:01:58 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/ForjenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForjenkinsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ForjenkinsApplication");
+		SpringApplication.run(ForjenkinsApplication.class, args);
+	}
+
+}
