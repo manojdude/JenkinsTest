e62ac4d5f04ea643162ddc1596657959e055e838
manojdude <68345729+manojdude@users.noreply.github.com>
2023-11-22 19:27:47 +0530

    Update ForjenkinsApplication.java
    for check1

--- src/main/java/com/example/demo/ForjenkinsApplication.java
+++ src/main/java/com/example/demo/ForjenkinsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ForjenkinsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("ForjenkinsApplication");
+		System.out.println("ForjenkinsApplication1");
 		SpringApplication.run(ForjenkinsApplication.class, args);
 	}
 
